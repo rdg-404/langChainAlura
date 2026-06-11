e480944db128fcd98e5ff7778b58433b5e745afd
rdg-404 <rodriigopaiiva.rp19@gmail.com>
2026-06-11 20:32:14 -0300

    obtenha resposta em json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from langchain.schema import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
 import os
 
@@ -8,12 +9,21 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 
+class Receita(BaseModel):
+    receita:str = Field("A receita a ser feita")
+    recomendada:str = Field("Para quem é recomendada a receita (Perder peso, ganhar massa, etc.)")
+
+parseador = JsonOutputParser(pydantic_object=Receita)
+
 
 prompt_receita = PromptTemplate(
     template=""""
-      Sugira receitas para perder peso. Dado meu interesse por {interesse}
+      Sugira receitas para perder peso. Dado meu interesse por {interesse}.
+      {formato_de_saida}
     """,
-    input_variables=["interesse"]
+
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
 
 
@@ -23,7 +33,7 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
-cadeia = prompt_receita | modelo | StrOutputParser()
+cadeia = prompt_receita | modelo | parseador
 
 resposta = cadeia.invoke(
     {"interesse": "frutas"}
